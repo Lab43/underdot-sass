b28debe9291bde43fc2398a85aab64cbbb42570d
Cameron Spencer <cameron@lab43.com>
2020-08-05 11:25:16 -0700

    use refactored plugin api

--- index.js
+++ index.js
@@ -2,20 +2,23 @@ const sass = require('node-sass');
 
 
 
-module.exports = (options = {}) => ({ registerFilePlugin }) => {
+module.exports = (options = {}) => (plugin) => {
 
-  registerFilePlugin(options.rule || '**/*.s@(a|c)ss', (path, buffer, next) => {
+  plugin.registerFileHandler(options.rule || '**/*.s@(a|c)ss', ({ path, file }) => {
     // rewrite file extension to .css
     const outputPath = path.split('.').slice(0, -1).concat(['css']).join('.');
     return new Promise((resolve, reject) => {
       sass.render({
         ...options,
-        data: buffer.toString('utf8'),
+        data: file.toString('utf8'),
         outFile: outputPath,
       }, async (err, result) => {
         if (err) return reject(err);
-        if (result.map) await next(path + '.map', result.map);
-        return next(path, result.css);
+        let output = {path: outputPath, file: result.css};
+        if (result.map) {
+          output = [output, {path: outputPath + '.map', file: result.map}]
+        }
+        resolve(output);
       });
     });
   });
